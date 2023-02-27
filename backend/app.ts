b5704a442fb01ccf8664c0a8f96ee5fb1dbfa141
RameshNeupane <neupaneramesh555@gmail.com>
2023-02-27 18:03:36 +0545

    [backend] update import path from relative to absolute for development

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,11 @@
-import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
-const PORT = process.env.PORT || 8000;
+import userRouter from "@routes/user";
+import configureDB from "@config/database";
+import { PORT } from "@config/environment";
 
 // express app instance
 const app = express();
@@ -13,26 +14,31 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
+app.use("/user", userRouter);
 
 // database connection
-const connectDB = () => {
-  try {
-    mongoose.connect(process.env.DATABASE_URI as string, () =>
-      console.log("database connected.")
-    );
-  } catch (error) {
-    console.error(error);
-  }
-};
-connectDB();
+configureDB();
 
 // root route
 app.get("/", (req, res) => {
   res.send({
-    message: "signup-mern",
+    app: "signup-mern",
   });
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`app running at http://localhost:${PORT}`);
 });
+
+// on signal interrupt
+const onSignalInterrupt = () => {
+  process.on("SIGINT", () => {
+    mongoose.connection.close(() => {
+      server.close(() => {
+        console.log("Express server closed");
+        process.exit(0);
+      });
+    });
+  });
+};
+onSignalInterrupt();
